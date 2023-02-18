9081812add99770eed5400569982dac08bb7b2d3
BirdsofaFeather <seyha.soun@gmail.com>
2023-02-18 16:12:52 -0500

    Update delete_multiple_chrome.sh

--- Scripts/delete_multiple_chrome.sh
+++ Scripts/delete_multiple_chrome.sh
@@ -17,8 +17,8 @@
 
 IFS=$'\n'
 
-currUser=$(ls -l /dev/console | awk '{print $3}')
-currUserID=$(id -u "$currUser")
+loggedInUser=$(ls -l /dev/console | awk '{print $3}')
+loggedInUserID=$(id -u "$loggedInUser")
 
 ## finds all Google Chrome in the Application folder and creates an array
 ChromeNames=( $(find /Applications -name "Google Chrome*.app" -maxdepth 1 -type d2) )
@@ -26,7 +26,7 @@ ChromeNames=( $(find /Applications -name "Google Chrome*.app" -maxdepth 1 -type 
 ## Checks to see if there are more than 1 Chrome in the Applications folder
 if [[ ${#ChromeNames[@]} > 1 ]] ; then
     echo "Found multiple instances of Chrome"
-    return=`/bin/launchctl asuser $currUserID /usr/bin/osascript <<-EOS
+    return=`/bin/launchctl asuser $loggedInUserID /usr/bin/osascript <<-EOS
     tell me to display dialog "You have multiple instances of Chrome installed.
 
 Please complete any tasks pending tasks in Chrome.
@@ -34,7 +34,7 @@ Please complete any tasks pending tasks in Chrome.
 Then click OK to close Chrome so that the other Chrome can be removed."
 EOS`
         if [[ "button returned:OK" = $return ]] ; then
-            /bin/launchctl asuser "$currUserID" /usr/bin/osascript -e 'tell application "Google Chrome" to quit'
+            /bin/launchctl asuser "$loggedInUserID" /usr/bin/osascript -e 'tell application "Google Chrome" to quit'
             sleep 3
 
             ## Delete all instances of Chrome besides the correct one.
@@ -46,7 +46,7 @@ EOS`
                     fi
             done
             sleep 3
-            /bin/launchctl asuser $currUserID /usr/bin/osascript <<-EOS
+            /bin/launchctl asuser $loggedInUserID /usr/bin/osascript <<-EOS
 tell me to display dialog "The other instances of Chrome have been deleted.
 
 You can now re-open Chrome."
